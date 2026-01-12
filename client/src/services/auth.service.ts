89c21df538bc1c320ab863812882dd350788b68b
PawanChandra2122 <pawanawesome16@gmail.com>
2026-01-12 09:32:49 +0000

    AuthService file done by Pawan

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -4,5 +4,58 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-//todo: complete missing code..
+
+  private token:string|null=null;
+  private isLoggedIn:boolean=false;
+
+  constructor(){
+
+  }
+  saveToken(token:string){
+    this.token=token;
+    this.isLoggedIn=true;
+    localStorage.setItem('token',token);
+  }
+
+  setRole(role:any){
+    localStorage.setItem('role',role);
+  }
+
+  get getRole():string|null{
+    return localStorage.getItem('role');
+  }
+
+  setUsername(username:any){
+    localStorage.setItem('username',username)
+  }
+
+  get getUsername ():string|null{
+    return localStorage.getItem('username');
+  }
+
+  setId(id:any)
+  {
+    localStorage.setItem('id',id);
+  }
+  
+  get getId ():string|null
+  {
+    return localStorage.getItem('id');
+  }
+
+  get getLoginStatus():boolean{
+    return !!localStorage.getItem('token');
+  }
+
+  getToken(): string | null {
+   this.token= localStorage.getItem('token');
+    return this.token;
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+     this.token=null;
+     this.isLoggedIn=false
+   }
 }
