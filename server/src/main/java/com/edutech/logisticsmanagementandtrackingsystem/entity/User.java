7baf446e33b451445c88650012e38997bdafff57
PawanChandra2122 <pawanawesome16@gmail.com>
2026-01-13 04:43:39 +0000

    EnitityAndDto-Pawan

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/entity/User.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/entity/User.java
@@ -1,10 +1,59 @@
 package com.edutech.logisticsmanagementandtrackingsystem.entity;
-
-
 import javax.persistence.*;
-
-
+@Entity
+@Table(name = "users")
 public class User {
-    // implement entity here
-}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="username")
+    private String username;
+    @Column(name="password")
+    private String password;
+    @Column(name="role")
+    private String role;
+    @Column(name="email") // Role can be 'BUSINESS', 'DRIVER', 'CUSTOMER'
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public String getUsername() {
+        return username;
+    }
+ 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+    public String getRole() {
+        return role;
+    }
+ 
+    public void setRole(String role) {
+        this.role = role;
+    }
+ 
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+ 
