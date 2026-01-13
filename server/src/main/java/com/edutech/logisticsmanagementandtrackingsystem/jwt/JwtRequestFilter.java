1ef744e18bce050a947f4bed84ae40100fcb7535
merilynjoyson <merilynjoysonnnn@gmail.com>
2026-01-13 04:45:44 +0000

    Completed jwt and config files

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/jwt/JwtRequestFilter.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/jwt/JwtRequestFilter.java
@@ -1,6 +1,6 @@
 package com.edutech.logisticsmanagementandtrackingsystem.jwt;
-
-
+ 
+ 
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -12,17 +12,60 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+ 
+import com.edutech.logisticsmanagementandtrackingsystem.service.UserService;
+ 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
-
-public class JwtRequestFilter {
-
-//Implement required code here!!!
-
-
+ 
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+ 
+    @Autowired
+    public JwtRequestFilter(@Lazy UserDetailsService userDetailsService, JwtUtil jwtUtil) {
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
+    }
+ 
+    
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+ 
+        String username = null;
+        String jwt = null;
+ 
+        
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+            
+        }
+ 
+       
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+ 
+            
+            if (jwtUtil.validateToken(jwt, userDetails)) {
+                Claims claims = jwtUtil.extractAllClaims(jwt);
+                Collection<? extends GrantedAuthority> authorities = AuthorityUtils
+                        .createAuthorityList((String) claims.get("role"));
+                
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+ 
+        filterChain.doFilter(request, response);
+    }
 }
--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/jwt/JwtUtil.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/jwt/JwtUtil.java
@@ -1,24 +1,108 @@
 package com.edutech.logisticsmanagementandtrackingsystem.jwt;
-
-
+ 
+ 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import com.edutech.logisticsmanagementandtrackingsystem.entity.User;
-import com.edutech.logisticsmanagementandtrackingsystem.repository.UserRepository;
-
+ 
+import java.security.Key;
+ 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
-
+ 
+import com.edutech.logisticsmanagementandtrackingsystem.entity.User;
+import com.edutech.logisticsmanagementandtrackingsystem.repository.UserRepository;
+ 
+ 
+@Component
 public class JwtUtil {
-
-//Implement required code here!!!
-
-
+ 
+    private UserRepository userRepository;
+ 
+    @Autowired
+    public JwtUtil(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+ 
+    
+    private final String secret = "secretKey000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+ 
+    
+    private final int expiration = 86400;
+ 
+    
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration * 1000);
+        User user = userRepository.findByUsername(username);
+ 
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("sub", username);
+ 
+        
+        claims.put("role", user.getRole());
+ 
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(getSignKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+ 
+    
+    public Claims extractAllClaims(String token) {
+        Claims claims;
+        try {
+            claims = Jwts
+                    .parserBuilder()
+                    .setSigningKey(getSignKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (Exception e) {
+            claims = null;
+        }
+        return claims;
+    }
+ 
+    
+    public String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+ 
+   
+    public boolean isTokenExpired(String token) {
+        Date expirationDate = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+        return expirationDate.before(new Date());
+    }
+ 
+    
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+ 
+    
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
