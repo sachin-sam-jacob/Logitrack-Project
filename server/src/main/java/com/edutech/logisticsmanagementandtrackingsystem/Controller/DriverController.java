4df514dcb230f44cc3624bf9b163baaeeb2ff1ae
sachin-sam-jacob <dummy@gmail.com>
2026-01-13 05:25:02 +0000

    Made Controller and Repository Completed

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/DriverController.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/Controller/DriverController.java
@@ -18,6 +18,28 @@ import java.util.List;
 public class DriverController {
 
     // Dependency Injections
-    
+    @Autowired
+    private DriverService driverService;
 
+    @GetMapping("/api/driver/cargo")
+    @PreAuthorize("hasAuthority('DRIVER')")
+    public ResponseEntity<List<Cargo>> viewAssignedCargos(@RequestParam Long driverId) {
+        // get assigned cargos for the driver and return with 200 OK
+        return new ResponseEntity<>(driverService.viewDriverCargos(driverId), HttpStatus.OK);
+    }
+
+    @PutMapping("api/driver/update-cargo-status")
+    @PreAuthorize("hasAuthority('DRIVER')")
+    public ResponseEntity<String> updateCargoStatus(@RequestParam Long cargoId, @RequestParam String newStatus) {
+        // update the cargo status
+        cargoId = (Long) cargoId;
+        boolean updateSuccess = driverService.updateCargoStatus(cargoId, newStatus);
+
+        if (updateSuccess) {
+            // if cargo updated sucessfully return this
+            return ResponseEntity.ok().body("{\"message\": \"Cargo status updated successfully.\"}");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update cargo status.");
+        }
+    }
 }
