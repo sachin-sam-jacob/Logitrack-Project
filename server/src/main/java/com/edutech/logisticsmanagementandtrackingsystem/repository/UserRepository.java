102ed2da28893c1e04b4b8a330ba0797721bff08
sachin-sam-jacob <dummy@gmail.com>
2026-01-21 05:26:48 +0000

    Made changes in the username of the login as it was not case sensitive

--- server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/repository/UserRepository.java
+++ server/src/main/java/com/edutech/logisticsmanagementandtrackingsystem/repository/UserRepository.java
@@ -4,13 +4,18 @@ package com.edutech.logisticsmanagementandtrackingsystem.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.edutech.logisticsmanagementandtrackingsystem.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User findByUsername(@Param("username") String username);
+
+
     boolean existsByEmailIgnoreCase(String email);
     Optional<User> findByEmailIgnoreCase(String email);
 
